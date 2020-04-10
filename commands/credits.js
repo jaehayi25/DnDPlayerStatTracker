07b5be9869d8e9b249a655be0ec951e1c388469b
Jaeha Yi <58238887+jaehayi25@users.noreply.github.com>
2020-04-10 09:38:22 -0600

    Delete credits.js

--- commands/credits.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'credits',
-    aliases: ['author', 'c', 'programmer'],
-	description: 'made in COVID-19',
-	argsName: false,
-	execute(message, commandArgs, Tags) {
-		message.channel.send('Made by Jaeha Yi - April 6, 2020. \nUpon request of Gavin Patino.');
-	},
-};
